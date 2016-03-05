526c6f3d932f272a86307db0f98624e22e0661e4
Adil Ansari <ansari.adil20@gmail.com>
2016-03-05 02:06:36 -0800

    Adding parse initializer to application

--- app/src/androidTest/java/com/adil/spott/ParseApplication.java
+++
@@ -1,8 +0,0 @@
-package com.adil.spott;
-
-import android.app.Application;
-
-public class ParseApplication extends Application {
-    public static final String PARSE_APPLICATION_ID = "spott_api";
-    public static final String PARSE_CLIENT_KEY = "fit_360";
-}
