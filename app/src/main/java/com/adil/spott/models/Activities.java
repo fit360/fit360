b75d199219d76a5c02975998286d7d85e694d80d
Adil Ansari <ansari.adil20@gmail.com>
2016-03-05 17:00:29 -0800

    adding activities model

---
+++ app/src/main/java/com/adil/spott/models/Activities.java
@@ -0,0 +1,52 @@
+package com.adil.spott.models;
+
+public enum Activities {
+    ARM_WRESTLING("Arm wrestling"),
+    BMX_BIKE_RIDING("BMX bike riding"),
+    BALLET("Ballet"),
+    BASEBALL("Baseball"),
+    BASKETBALL("Basketball"),
+    BICYCLING("Bicycling"),
+    BOXING("Boxing"),
+    CALISTHENICS("Calisthenics"),
+    CANOEING("Canoeing"),
+    KAYAKING("Kayaking"),
+    ROWING("Rowing"),
+    CROSSFIT("Crossfit"),
+    DIVING("Diving"),
+    FOOTBALL("Football"),
+    GYMNASTICS("Gymnastics"),
+    HOCKEY("Hockey"),
+    RUNNING("Running"),
+    KUNG_FU("Kung-fu"),
+    MARATHON_RUNNING("Marathon running"),
+    POLE_DANCING("Pole dancing"),
+    POLE_VAULTING("Pole vaulting"),
+    POWERLIFTING("Powerlifting"),
+    ROCK_CLIMBING("Rock climbing"),
+    SKATEBOARDING("Skateboarding"),
+    ICE_SKATING("Ice skating"),
+    SKIING("Skiing"),
+    SNOWBOARDING("Snowboarding"),
+    SOCCER("Soccer"),
+    SPRINTING("Sprinting"),
+    STRETCHING("Stretching"),
+    SUPER_SQUATS("Super squats"),
+    SURFING("Surfing"),
+    SWIMMING("Swimming"),
+    SWORD_FIGHTING("Sword fighting"),
+    TAI_CHI("Tai-chi"),
+    TENNIS("Tennis"),
+    VOLLEYBALL("Volleyball"),
+    WALKING("Walking"),
+    WATER_AEROBICS("Water aerobics"),
+    WATER_POLO("Water polo"),
+    WRESTLING("Wrestling"),
+    YOGA("Yoga"),
+    ZUMBA("Zumba");
+
+    private String name;
+
+    Activities(String name){this.name = name;}
+
+    }
--- app/src/main/java/com/adil/spott/models/Activity.java
+++ app/src/main/java/com/adil/spott/models/Activity.java
@@ -3,6 +3,11 @@ package com.adil.spott.models;
 public class Activity extends Model{
 
     private static final String TAG = Activity.class.getSimpleName();
+    private static final String ACTIVITY = "activity";
+    private static final String USER = "user";
+    private static final String TYPE = "type";
+    private static final String TIME = "time";
+    private static final String FREQUENCY = "frequency";
 
     @Override
     public String getLogTag() {
