6dd05d95b33fa3806f89c1c6c31a8971f2c5d450
Aparna Jain <aparnajain@Aparnas-MacBook-Pro.local>
2016-03-11 22:57:56 -0800

    refresh on community feeds

--- app/src/main/java/com/app/spott/activities/CommunityFeedActivity.java
+++ app/src/main/java/com/app/spott/activities/CommunityFeedActivity.java
@@ -1,6 +1,8 @@
 package com.app.spott.activities;
 
 import android.os.Bundle;
+import android.support.v4.widget.SwipeRefreshLayout;
+import android.support.v4.widget.SwipeRefreshLayout.OnRefreshListener;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
@@ -21,11 +23,28 @@ public class CommunityFeedActivity extends AppCompatActivity {
     ArrayList<Post> mPosts;
     RecyclerView rvPosts;
     LinearLayoutManager linearLayoutManager;
+    private SwipeRefreshLayout swipeContainer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_community_feed);
+
+        swipeContainer = (SwipeRefreshLayout) findViewById(R.id.swipeContainer);
+        swipeContainer.setOnRefreshListener(new OnRefreshListener() {
+            @Override
+            public void onRefresh() {
+                // Your code to refresh the list here.
+                // Make sure you call swipeContainer.setRefreshing(false)
+                // once the network request has completed successfully.
+                fetchFeeds();
+            }
+        });
+        swipeContainer.setColorSchemeResources(android.R.color.holo_blue_bright,
+                android.R.color.holo_green_light,
+                android.R.color.holo_orange_light,
+                android.R.color.holo_red_light);
+
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         mPosts = new ArrayList<>();
@@ -46,7 +65,7 @@ public class CommunityFeedActivity extends AppCompatActivity {
                     // Access data using the `getGender` methods for the object
                     mPosts.addAll((ArrayList)posts);
                     aPosts.notifyDataSetChanged();
-                    // Do whatever you want with the data...
+                    swipeContainer.setRefreshing(false);
                 } else {
                     // something went wrong
                 }
