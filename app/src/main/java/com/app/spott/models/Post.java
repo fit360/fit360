37b4bae5b90f0340859781157b424a724fbde2b6
Adil Ansari <ansari.adil20@gmail.com>
2016-03-23 13:04:36 -0700

    chat fixes

--- app/src/main/java/com/app/spott/models/Post.java
+++ app/src/main/java/com/app/spott/models/Post.java
@@ -11,7 +11,7 @@ import com.parse.ParseQuery;
 public class Post extends Model {
     private static final String USER = "user";
     private static final String IMAGE_URL = "image_url";
-    private static final String BODY = "tvMyMessage";
+    private static final String BODY = "body";
     private static final String CREATED_AT = "createdAt";
 
     public Post() {
